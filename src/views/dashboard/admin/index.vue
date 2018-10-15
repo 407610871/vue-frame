0b9ca658bef030494f68586e0025ea48bcc69c94
叶楚楠 <yechunan@encdata.cn>
2018-10-15 16:38:59 +0800

    first commit

---
+++ src/views/dashboard/admin/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dashboard-container">
+    123
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'DashboardAdmin',
+  components: {
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.dashboard-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+}
+</style>
---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="classObj" class="app-wrapper">
+    <div class="main-container">
+      <app-main />
+    </div>
+  </div>
+</template>
+
+<script>
+import { AppMain } from './components'
+
+export default {
+  name: 'Layout',
+  components: {
+    AppMain
+  },
+  computed: {
+  
+  },
+  methods: {
+   
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "src/styles/mixin.scss";
+  .app-wrapper {
+    @include clearfix;
+    position: relative;
+    height: 100%;
+    width: 100%;
+  }
+
+</style>
