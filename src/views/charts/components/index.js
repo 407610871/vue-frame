dd973ec6c7043294e18a1be9d39e7e06d3a4b89b
Chih-Hao <zhihaozhang@me.com>
2018-10-24 17:10:34 +0800

    add HorizontalBarchart Component

--- src/views/charts/components/index.js
+++ src/views/charts/components/index.js
@@ -6,3 +6,5 @@ export { default as PieChart } from "../../charts/components/PieChart/index.vue"
 export { default as BarChart } from "../../charts/components/BarChart/index.vue";
 
 export { default as MultiLinechart } from "../../charts/components/MultiLinechart/index.vue";
+
+export { default as HorizontalBarchart } from "../../charts/components/HorizontalBarchart/index.vue";
