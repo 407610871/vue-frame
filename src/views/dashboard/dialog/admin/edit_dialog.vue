e0a1b49dcc6fd2f3bdc5382e23fdfc68170f95cb
chenxiangxu <chenxiangxu@encdata.cn>
2018-11-12 18:07:22 +0800

    图标

--- src/views/dashboard/dialog/admin/edit_dialog.vue
+++ src/views/dashboard/dialog/admin/edit_dialog.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="taskMDialog">
-    <el-button class="diabtn incbtn" size="mini" @click="dialogVisible = true">编辑</el-button>
+    <!-- <el-button class="diabtn incbtn" size="mini" @click="dialogVisible = true">编辑</el-button> -->
+     <i title="编辑" @click="dialogVisible = true" class="enc-icon-bianji table-action-btn"></i>
     <el-dialog title="接入数据源" :visible.sync="dialogVisible" width="60%" :before-close="closeDialog">
       <div class="title-gra">
         <span class="grab gra-l"></span>
