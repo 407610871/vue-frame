52cd75e7e5d9de59afa59cde7745da40ddb10b8e
chenxiangxu <chenxiangxu@encdata.cn>
2018-11-20 17:32:30 +0800

    修改表名

--- src/views/accessObjManage/dialog/admin/set_task.vue
+++ src/views/accessObjManage/dialog/admin/set_task.vue
@@ -2,7 +2,7 @@
   <div class="taskMDialog userSurveyDialog setTaskDia">
    <!--  <el-button @click="setTask()" class="add-btn fr">批量采集</el-button> -->
    <el-tooltip class="item" effect="light" content="批量采集" placement="top">
-   <span class="talogo diabtn tin-btn add-btn" @click="dialogVisible = true"></span>
+   <span class="talogo diabtn tin-btn add-btn" @click="setTask()"></span>
    </el-tooltip>
     <!--  <i class="el-icon-info" @click="dialogVisible = true">设置通配符</i> -->
     <el-dialog title="批量接入任务向导" :visible.sync="dialogVisible" width="73%" :before-close="closeDialog">
