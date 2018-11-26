95d17d18ca3692f7b650d95683a8703032218d8d
chenxiangxu <chenxiangxu@encdata.cn>
2018-11-26 12:54:59 +0800

    修改问题单

--- src/views/mainLay/dialog/com_map.vue
+++ src/views/mainLay/dialog/com_map.vue
@@ -27,7 +27,7 @@
       </el-table>
     </div>
     <div class="btn tcenter mt30">
-      <!--  <el-button type="primary" style="margin-top: 12px;" @click="pre()">上一步</el-button> -->
+      <el-button type="primary" style="margin-top: 12px;" @click="pre()">上一步</el-button>
       <el-button type="primary" style="margin-top: 12px;" @click="next()">下一步</el-button>
     </div>
   </div>
@@ -61,24 +61,51 @@ export default {
     _getMatch() {
       var _self = this;
       _self.tableData = [];
+      console.log(_self.$store.state.schemaList);
       _self.schemaMappingDTOList = [];
       if (_self.$store.state.noreData.length == 0) {
 
       } else {
         _self.tableData = _self.$store.state.noreData;
+        if (_self.$store.state.schemaList.length != 0) {
+          _self.schemaMappingDTOList = _self.$store.state.schemaList;
+          for (let i = 0; i < _self.tableData.length; i++) {
+            let sflag = true;
+            for (let n = 0; n < _self.schemaMappingDTOList.length; n++) {
+              if (_self.schemaMappingDTOList[n].length == _self.tableData[i].id) {
+                sflag = true;
+                break;
+              } else {
+                sflag = false;
+              }
+            }
+            if (sflag == false) {
+              _self.schemaMappingDTOList.push({
+                "newColumnName": _self.tableData[i].name,
+                "newColumnType": '',
+                "orgColumnName": _self.tableData[i].name,
+                "orgColumnType": _self.tableData[i].datatype,
+                "orgColumnComment": _self.tableData[i].comments,
+                "length": _self.tableData[i].id
+              })
+            }
+          }
+        } else {
+          for (let j = 0; j < _self.tableData.length; j++) {
+            _self.schemaMappingDTOList.push({
+              "newColumnName": _self.tableData[j].name,
+              "newColumnType": '',
+              "orgColumnName": _self.tableData[j].name,
+              "orgColumnType": _self.tableData[j].datatype,
+              "orgColumnComment": _self.tableData[j].comments,
+              "length": _self.tableData[j].id
+            })
+          }
 
-        for (let j = 0; j < _self.tableData.length; j++) {
-          _self.schemaMappingDTOList.push({
-            "newColumnName": _self.tableData[j].name,
-            "newColumnType": '',
-            "orgColumnName": _self.tableData[j].name,
-            "orgColumnType": _self.tableData[j].datatype,
-            "orgColumnComment": _self.tableData[j].comments,
-            "length": _self.tableData[j].length
-          })
         }
-        // console.log( _self.tableData)
         this._getAllType();
+        // console.log( _self.tableData)
+
       }
 
     },
@@ -114,6 +141,7 @@ export default {
     _getAllType() {
 
       var _self = this;
+      _self.cloneData = [];
       if (_self.$store.state.isParquet) {
         for (let m = 0; m < columnJson.length; m++) {
           if (_self.$route.params.type == columnJson[m].type) {
@@ -200,7 +228,7 @@ export default {
               )
             }
           } else {
-            if (_self.tableData[i].datatype.toUpperCase().indexOf('INT')!=-1) {
+            if (_self.tableData[i].datatype.toUpperCase().indexOf('INT') != -1) {
               _self.cloneData.push(
                 'BIGINT'
               )
@@ -215,7 +243,10 @@ export default {
         }
       }
       for (let n = 0; n < _self.cloneData.length; n++) {
-        _self.schemaMappingDTOList[n].newColumnType = _self.cloneData[n];
+       if(_self.schemaMappingDTOList[n].newColumnType==''){
+         _self.schemaMappingDTOList[n].newColumnType = _self.cloneData[n];
+       }
+       
       }
       console.log(_self.schemaMappingDTOList);
       console.log(_self.cloneData);
@@ -224,7 +255,9 @@ export default {
     },
     //上一步
     pre() {
-      /*this.$emit('pre');*/
+      this.$emit('pre');
+      this.setSchemaList(this.schemaMappingDTOList);
+      console.log(this.$store.state.schemaList);
     },
     //下一步
     next() {
