b437ba64f2005928b96b2e4ccb32e97cc298c29e
叶楚楠 <yechunan@ennew.cn>
2018-11-28 12:22:00 +0800

    实验室演示用数据处理功能展示

---
+++ src/views/dataProcessing/index.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="data-processing"></div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'DataProcessing',
+  components: {},
+}
+</script>
+
+<style scoped>
+  .data-processing {
+    width: 100%;
+    height: calc(100vh - 66px)!important;
+    background: url('../../assets/images/sjcl.jpg') no-repeat;
+    background-size: cover;
+  }
+</style>
