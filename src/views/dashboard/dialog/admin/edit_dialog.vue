de4ed400b04cd4d1951297283d8733340107e823
chenxiangxu <chenxiangxu@encdata.cn>
2018-12-04 13:39:50 +0800

    测试连接

--- src/views/dashboard/dialog/admin/edit_dialog.vue
+++ src/views/dashboard/dialog/admin/edit_dialog.vue
@@ -207,8 +207,8 @@
               <el-col :span="2" class="bank" v-if="ruleForm.syskind=='10002'">bank</el-col>
               <el-col :span="10" class="uncol">
                 <span class="fl" v-show="this.ruleForm.author=='true'&&(ruleForm.syskind=='10002')">*</span>
-                <el-form-item class="fl unrequired" label="模式名/架构/Schema:" prop="model" v-if="ruleForm.syskind=='10002'">
-                  <el-input v-model="ruleForm.model" @change="changeContentInfo()"></el-input>
+                <el-form-item class="fl unrequired" label="模式名/架构/Schema:" prop="username" v-if="ruleForm.syskind=='10002'">
+                  <el-input v-model="ruleForm.username" disabled @change="changeContentInfo()"></el-input>
                 </el-form-item>
               </el-col>
               <!--  <el-col :span="10">
@@ -751,7 +751,7 @@ export default {
               },
               {
                 "key": "model",
-                "value": _self.ruleForm.model
+                "value": _self.ruleForm.username
               },
               {
                 "key": "linkman",
@@ -904,7 +904,7 @@ export default {
             "ip": this.ruleForm.ipname,
             "port": this.ruleForm.iport,
             "instanceName": this.ruleForm.instanceName,
-            "model": this.ruleForm.model,
+            "model": this.ruleForm.username,
             "username": this.ruleForm.username,
             "password": this.ruleForm.password,
             "url": this.ruleForm.url,
