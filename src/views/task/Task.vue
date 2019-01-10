c035df6497d1dead91d0d453ed5580aba7c35002
chenxiangxu <chenxiangxu@encdata.cn>
2019-01-10 13:43:38 +0800

    任务

--- src/views/task/Task.vue
+++ src/views/task/Task.vue
@@ -488,14 +488,13 @@ export default {
     doConverge(index, row) {
       let _self = this;
       _self.loading = true;
-
       this.$ajax
         .put(httpUrl + "manager/taskOperate/converge/" + row.taskInfoId)
         .then(function(res) {
           _self.loading = false;
           if (res.data.success) {
             _self.doMsg(
-              "汇聚任务已经生成，任务将在接下来的周期执行",
+              "汇聚任务ID:" + row.taskInfoId + "重新汇聚任务创建成功！",
               "success"
             );
             _self.init();
