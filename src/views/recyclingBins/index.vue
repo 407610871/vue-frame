b5c3c4052a0ac57f15286302f07e711b582b62a5
chenxiangxu <chenxiangxu@encdata.cn>
2019-01-18 19:24:46 +0800

    回收箱

--- src/views/recyclingBins/index.vue
+++ src/views/recyclingBins/index.vue
@@ -5,7 +5,7 @@
               回收箱
       </el-breadcrumb-item>
     </el-breadcrumb>
-    <el-container class="dashboard-container main-content" v-loading="loading">
+    <div class="dashboard-container main-content" v-loading="loading">
       <div class="filter-container" :height="headerHeight">
         <a v-on:click="collapseExpand" class="right-btn collapse-btn"><i :class="{'enc-icon-zhankai':collapse,'enc-icon-shousuo':!collapse}"></i></a>
         <formFliter v-if="queryParamReady" v-bind:formCollapse="collapse" v-bind:dataObj="formFilterData" @formFilter="changeFormFilter" />
@@ -51,7 +51,7 @@
           </el-pagination>
         </div>
       </el-footer>
-    </el-container>
+    </div>
   </div>
 </template>
 <script>
